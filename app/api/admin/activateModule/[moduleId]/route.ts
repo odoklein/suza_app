a790f710c57ae044c055511766ccba46c80b618a
Slimani Mohammed <150338098+odoklein@users.noreply.github.com>
2025-03-21 01:34:28 +0100

    Update route.ts

--- app/api/admin/activateModule/[moduleId]/route.ts
+++ app/api/admin/activateModule/[moduleId]/route.ts
@@ -7,23 +7,32 @@ export async function POST(req: Request, props: { params: Promise<{ moduleId: st
   const params = await props.params;
   const session = await getServerSession(authOptions);
 
+  console.log("POST function started for moduleId:", await params.moduleId);
+
   if (!session) {
+    console.log("User not authenticated.");
     return new NextResponse("Unauthenticated", { status: 401 });
   }
+
+  console.log("User authenticated.");
+  console.log("Attempting to connect to the database...");
 
   try {
     const user = await prismadb.system_Modules_Enabled.update({
       where: {
-        id: params.moduleId,
+        id: await params.moduleId,
       },
       data: {
         enabled: true,
       },
     });
 
+    console.log("Database interaction successful. Module activated.");
+    console.log("Sending response...");
+
     return NextResponse.json(user);
   } catch (error) {
-    console.log("[USERACTIVATE_POST]", error);
+    console.error("Error in POST function:", error);
     return new NextResponse("Initial error", { status: 500 });
   }
 }
